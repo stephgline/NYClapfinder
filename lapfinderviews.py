7ef8e59386dbef2ff4add0334ffb9890ab8e1ad5
stephgline <stephgline@gmail.com>
2016-04-29 12:28:51 -0400

    added drop down option

--- lapfinderviews.py
+++ lapfinderviews.py
@@ -6,13 +6,20 @@ from app import app
 def lapfinder():
 	return render_template("lapfinderindex.html")
 
-
 @app.route('/lapfinderoutput')
 def lapfinderoutput():
-	startaddress = request.args.get('LFID')
-	closepool, pooladdress, laphours, today, website, hours = zipcounter.addresscoord(startaddress)
-	
-	return render_template("lapfinderoutput.html", closepool=closepool, pooladdress=pooladdress, laphours =laphours, today=today, website=website, hours = hours)
-	
+	#getting entered address
+	try:
+		startaddress = request.args.get('LFID')
+		closepool, pooladdress, laphours, today, website, hours = zipcounter.addresscoord(startaddress)
 
-
+		return render_template("lapfinderoutput.html", closepool=closepool, pooladdress=pooladdress, laphours =laphours, today=today, website=website, hours = hours)
+	#getting direct pool choice from dropdown
+	except:
+		poolchoice = request.args.get('pool')
+		if poolchoice == 'None':
+			return render_template("lapfinderindexnone.html")
+		else:
+			closepool, pooladdress, laphours, today, website, hours = zipcounter.directpool(poolchoice)
+		
+			return render_template("lapfinderdirectoutput.html", closepool=closepool, pooladdress=pooladdress, laphours =laphours, today=today, website=website, hours = hours)
